d89314b1fda6fd030da85e4b8a040f7a9e90624c
RedstoneParadox <spinjitzu00@gmail.com>
2022-01-24 17:17:03 -0800

    Finish setup

---
+++ src/main/java/io/github/redstoneparadox/betterenchantmentboosting/BetterEnchantmentBoosting.java
@@ -0,0 +1,10 @@
+package io.github.redstoneparadox.betterenchantmentboosting;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BetterEnchantmentBoosting implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("Hello, world!");
+	}
+}
