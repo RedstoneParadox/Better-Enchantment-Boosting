f10a69f4e8759cd4affd7dd065ab5c143367331b
RedstoneParadox <spinjitzu00@gmail.com>
2022-01-24 23:40:35 -0800

    Registered bookshelves and candles to enchantment power registry

--- src/main/java/io/github/redstoneparadox/betterenchantmentboosting/BetterEnchantmentBoosting.java
+++ src/main/java/io/github/redstoneparadox/betterenchantmentboosting/BetterEnchantmentBoosting.java
@@ -1,10 +1,39 @@
 package io.github.redstoneparadox.betterenchantmentboosting;
 
+import io.github.redstoneparadox.betterenchantmentboosting.util.EnchantmentPowerRegistry;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.CandleBlock;
 
 public class BetterEnchantmentBoosting implements ModInitializer {
 	@Override
 	public void onInitialize() {
-		System.out.println("Hello, world!");
+		EnchantmentPowerRegistry.register(Blocks.BOOKSHELF.getDefaultState(), 1.0f);
+		EnchantmentPowerRegistry.register(Blocks.CANDLE.getDefaultState().with(CandleBlock.CANDLES, 4), 1.0f);
+
+		Block[] candleBlocks = new Block[] {
+				Blocks.CANDLE,
+				Blocks.WHITE_CANDLE,
+				Blocks.ORANGE_CANDLE,
+				Blocks.MAGENTA_CANDLE,
+				Blocks.LIGHT_BLUE_CANDLE,
+				Blocks.YELLOW_CANDLE,
+				Blocks.LIME_CANDLE,
+				Blocks.PINK_CANDLE,
+				Blocks.GRAY_CANDLE,
+				Blocks.LIGHT_GRAY_CANDLE,
+				Blocks.CYAN_CANDLE,
+				Blocks.PURPLE_CANDLE,
+				Blocks.BLUE_CANDLE,
+				Blocks.BROWN_CANDLE,
+				Blocks.GREEN_CANDLE,
+				Blocks.RED_CANDLE,
+				Blocks.BLACK_CANDLE
+		};
+
+		for (Block candleBlock: candleBlocks) {
+			EnchantmentPowerRegistry.register(candleBlock.getDefaultState().with(CandleBlock.CANDLES, 4), 1.0f);
+		}
 	}
 }
