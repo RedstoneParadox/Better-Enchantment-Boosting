49b12c8ef316633f4667eb05851169f824e2b64d
RedstoneParadox <spinjitzu00@gmail.com>
2022-01-24 23:33:27 -0800

    Create EnchantmentPowerRegistry.java

---
+++ src/main/java/io/github/redstoneparadox/betterenchantmentboosting/util/EnchantmentPowerRegistry.java
@@ -0,0 +1,19 @@
+package io.github.redstoneparadox.betterenchantmentboosting.util;
+
+import net.minecraft.block.BlockState;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EnchantmentPowerRegistry {
+	private static final Map<BlockState, Float> REGISTRY = new HashMap<>();
+
+	public static void register(BlockState state, float power) {
+		REGISTRY.put(state, power);
+	}
+
+	public static float getPower(BlockState state) {
+		if (REGISTRY.containsKey(state)) return REGISTRY.get(state);
+		return 0;
+	}
+}
