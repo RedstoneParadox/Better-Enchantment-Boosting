1014d9fa08d885aab99e1338795da0a14c3a0977
RedstoneParadox <spinjitzu00@gmail.com>
2022-01-24 22:44:02 -0800

    Made a better algorithm
    Now uses a 3D array instead of iterating through a single growing list, which helps when checking if neighboring cells are empty

--- src/main/java/io/github/redstoneparadox/betterenchantmentboosting/search/SearchArea.java
+++ src/main/java/io/github/redstoneparadox/betterenchantmentboosting/search/SearchArea.java
@@ -1,6 +1,5 @@
 package io.github.redstoneparadox.betterenchantmentboosting.search;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
@@ -9,6 +8,7 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -16,7 +16,6 @@ import java.util.Set;
 public final class SearchArea {
 	private GrowthPredicate growthPredicate = block -> false;
 	private SearchPredicate searchPredicate = block -> false;
-	private final Set<SearchNode> nodes = new HashSet<>();
 
 	public void setGrowthPredicate(GrowthPredicate growthPredicate) {
 		this.growthPredicate = growthPredicate;
@@ -27,59 +26,57 @@ public final class SearchArea {
 	}
 
 	public List<BlockPos> search(World world, BlockPos origin, Box bounds) {
-		List<BlockPos> matchesList = new ArrayList<>();
+		List<BlockPos> matches = new ArrayList<>();
+		int minX = (int) (bounds.minX - origin.getX());
+		int minY = (int) (bounds.minY - origin.getY());
+		int minZ = (int) (bounds.minZ - origin.getZ());
+		int maxX = (int) (bounds.maxX - origin.getX());
+		int maxY = (int) (bounds.maxY - origin.getY());
+		int maxZ = (int) (bounds.maxZ - origin.getZ());
+		CellArray cells = new CellArray(minX, minY, minZ, maxX, maxY, maxZ);
 
-		nodes.clear();
-		nodes.add(new SearchNode(0, 0, 0));
-
+		System.out.println("Growing!");
 		while (true) {
-			List<SearchNode> newNodes = new ArrayList<>();
+			int growingCount = 0;
 
-			for (SearchNode node: nodes) {
-				if (!node.dead) {
-					for (Direction direction: Direction.values()) {
-						boolean positionOccupied = false;
-						int nxtX = direction.getOffsetX() + node.x;
-						int nxtY = direction.getOffsetY() + node.y;
-						int nxtZ = direction.getOffsetZ() + node.z;
+			for (int x = minX; x <= maxX; x++) {
+				for (int y = minY; y <= maxY; y++) {
+					for (int z = minZ; z <= maxZ; z++) {
+						if (cells.get(x, y, z) != CellState.GROWING) continue;
 
-						for (SearchNode node2: nodes) {
-							if (node2.x == nxtX && node2.y == nxtY && node2.z == nxtZ) {
-								positionOccupied = true;
-								break;
-							}
-						}
+						for (Direction direction: Direction.values()) {
+							int nxtX = direction.getOffsetX() + x;
+							int nxtY = direction.getOffsetY() + y;
+							int nxtZ = direction.getOffsetZ() + z;
 
-						if (!positionOccupied) {
 							BlockPos blockPos = origin.add(nxtX, nxtY, nxtZ);
 
-							if (bounds.contains(Vec3d.ofCenter(blockPos))) {
+							if (bounds.contains(Vec3d.ofCenter(blockPos)) && cells.get(nxtX, nxtY, nxtZ) == CellState.EMPTY) {
 								BlockState state = world.getBlockState(blockPos);
-								SearchNode nextNode = new SearchNode(nxtX, nxtY, nxtZ);
-								newNodes.add(nextNode);
 
 								if (searchPredicate.isMatch(state)) {
-									matchesList.add(blockPos);
-									nextNode.dead = true;
+									cells.set(nxtX, nxtY, nxtZ, CellState.GROWN);
+									matches.add(blockPos);
 								} else if (!growthPredicate.canGrow(state)) {
-									nextNode.dead = true;
+									cells.set(nxtX, nxtY, nxtZ, CellState.GROWN);
+								} else {
+									cells.set(nxtX, nxtY, nxtZ, CellState.GROWING);
+									growingCount += 1;
 								}
 							}
 						}
+
+						// Cell no longer needs to grow in future iterations
+						cells.set(x, y, z, CellState.GROWN);
 					}
 				}
-
-				node.dead = true;
 			}
 
-			if (newNodes.isEmpty()) {
-				break;
-			}
-
-			nodes.addAll(newNodes);
+			System.out.println("growingCount = " + growingCount);
+			if (growingCount == 0) break;
 		}
 
-		return matchesList;
+		return matches;
 	}
 
 	public interface GrowthPredicate {
@@ -88,5 +85,48 @@ public final class SearchArea {
 
 	public interface SearchPredicate {
 		boolean isMatch(BlockState state);
+	}
+
+	private enum CellState {
+		EMPTY,
+		GROWING,
+		GROWN
+	}
+
+	private static class CellArray {
+		final int minX;
+		final int minY;
+		final int minZ;
+		List<List<List<CellState>>> cellStates = new ArrayList<>();
+
+		private CellArray(int minX, int minY, int minZ, int maxX, int maxY, int maxZ) {
+			this.minX = minX;
+			this.minY = minY;
+			this.minZ = minZ;
+
+			for (int x = minX; x <= maxX; x++) {
+				List<List<CellState>> yList = new ArrayList<>();
+
+				for (int y = minY; y <= maxY; y++) {
+					List<CellState> zList = new ArrayList<>();
+
+					for (int z = minZ; z <= maxZ; z++) {
+						if (x == 0 && y == 0 && z == 0) zList.add(CellState.GROWING);
+						else zList.add(CellState.EMPTY);
+					}
+
+					yList.add(zList);
+				}
+				cellStates.add(yList);
+			}
+		}
+
+		private CellState get(int x, int y, int z) {
+			return cellStates.get(x - minX).get(y - minY).get(z - minZ);
+		}
+
+		private void set(int x, int y, int z, CellState cellState) {
+			cellStates.get(x - minX).get(y - minY).set(z - minZ, cellState);
+		}
 	}
 }
