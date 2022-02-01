e45024ea9b1b32c8b8f8b2ecd35548604b4199a2
RedstoneParadox <spinjitzu00@gmail.com>
2022-01-31 22:13:04 -0800

    Revert "Used the CANDLES block tag instead of an array"
    This reverts commit 1570e04e141cb1523fcc0f03f0c862dba2e243ff.

--- src/main/java/io/github/redstoneparadox/betterenchantmentboosting/BetterEnchantmentBoosting.java
+++ src/main/java/io/github/redstoneparadox/betterenchantmentboosting/BetterEnchantmentBoosting.java
@@ -33,39 +33,55 @@ public class BetterEnchantmentBoosting implements ModInitializer {
 		}
 
 		if (CONFIG.candleBoosting()) {
-			Tag<Block> candles = BlockTags.CANDLES;
+			Block[] candleBlocks = new Block[] {
+					Blocks.CANDLE,
+					Blocks.WHITE_CANDLE,
+					Blocks.ORANGE_CANDLE,
+					Blocks.MAGENTA_CANDLE,
+					Blocks.LIGHT_BLUE_CANDLE,
+					Blocks.YELLOW_CANDLE,
+					Blocks.LIME_CANDLE,
+					Blocks.PINK_CANDLE,
+					Blocks.GRAY_CANDLE,
+					Blocks.LIGHT_GRAY_CANDLE,
+					Blocks.CYAN_CANDLE,
+					Blocks.PURPLE_CANDLE,
+					Blocks.BLUE_CANDLE,
+					Blocks.BROWN_CANDLE,
+					Blocks.GREEN_CANDLE,
+					Blocks.RED_CANDLE,
+					Blocks.BLACK_CANDLE
+			};
 
-			for (Block candleBlock: candles.values()) {
-				if (candleBlock instanceof CandleBlock) {
-					EnchantmentPowerRegistry.register(
-							candleBlock
-									.getDefaultState()
-									.with(CandleBlock.CANDLES, 4)
-									.with(CandleBlock.LIT, true),
-							CONFIG.powerPerCandle() * 4
-					);
-					EnchantmentPowerRegistry.register(
-							candleBlock
-									.getDefaultState()
-									.with(CandleBlock.CANDLES, 3)
-									.with(CandleBlock.LIT, true),
-							CONFIG.powerPerCandle() * 3
-					);
-					EnchantmentPowerRegistry.register(
-							candleBlock
-									.getDefaultState()
-									.with(CandleBlock.CANDLES, 2)
-									.with(CandleBlock.LIT, true),
-							CONFIG.powerPerCandle() * 2
-					);
-					EnchantmentPowerRegistry.register(
-							candleBlock
-									.getDefaultState()
-									.with(CandleBlock.CANDLES, 1)
-									.with(CandleBlock.LIT, true),
-							CONFIG.powerPerCandle() * 1
-					);
-				}
+			for (Block candleBlock: candleBlocks) {
+				EnchantmentPowerRegistry.register(
+						candleBlock
+								.getDefaultState()
+								.with(CandleBlock.CANDLES, 4)
+								.with(CandleBlock.LIT, true),
+						CONFIG.powerPerCandle() * 4
+				);
+				EnchantmentPowerRegistry.register(
+						candleBlock
+								.getDefaultState()
+								.with(CandleBlock.CANDLES, 3)
+								.with(CandleBlock.LIT, true),
+						CONFIG.powerPerCandle() * 3
+				);
+				EnchantmentPowerRegistry.register(
+						candleBlock
+								.getDefaultState()
+								.with(CandleBlock.CANDLES, 2)
+								.with(CandleBlock.LIT, true),
+						CONFIG.powerPerCandle() * 2
+				);
+				EnchantmentPowerRegistry.register(
+						candleBlock
+								.getDefaultState()
+								.with(CandleBlock.CANDLES, 1)
+								.with(CandleBlock.LIT, true),
+						CONFIG.powerPerCandle() * 1
+				);
 			}
 		}
 	}
