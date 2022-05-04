03d0c1a2e531513d59d79f665d384dfd2b8c3888
RedstoneParadox <spinjitzu00@gmail.com>
2022-05-04 16:12:32 -0700

    Candles now register with tags

--- src/main/java/io/github/redstoneparadox/betterenchantmentboosting/BetterEnchantmentBoosting.java
+++ src/main/java/io/github/redstoneparadox/betterenchantmentboosting/BetterEnchantmentBoosting.java
@@ -27,62 +27,38 @@ public class BetterEnchantmentBoosting implements ModInitializer {
 
 				if (state.isIn(Tags.BOOKSHELVES)) {
 					registryFunction.register(state, 1.0);
+				} else if (state.isIn(BlockTags.CANDLES) && block instanceof CandleBlock && CONFIG.candleBoosting()) {
+					registryFunction.register(
+							block
+									.getDefaultState()
+									.with(CandleBlock.CANDLES, 4)
+									.with(CandleBlock.LIT, true),
+							CONFIG.powerPerCandle() * 4
+					);
+					registryFunction.register(
+							block
+									.getDefaultState()
+									.with(CandleBlock.CANDLES, 3)
+									.with(CandleBlock.LIT, true),
+							CONFIG.powerPerCandle() * 3
+					);
+					registryFunction.register(
+							block
+									.getDefaultState()
+									.with(CandleBlock.CANDLES, 2)
+									.with(CandleBlock.LIT, true),
+							CONFIG.powerPerCandle() * 2
+					);
+					registryFunction.register(
+							block
+									.getDefaultState()
+									.with(CandleBlock.CANDLES, 1)
+									.with(CandleBlock.LIT, true),
+							CONFIG.powerPerCandle() * 1
+					);
 				}
 			}
 		}));
-
-		if (CONFIG.candleBoosting()) {
-			Block[] candleBlocks = new Block[] {
-					Blocks.CANDLE,
-					Blocks.WHITE_CANDLE,
-					Blocks.ORANGE_CANDLE,
-					Blocks.MAGENTA_CANDLE,
-					Blocks.LIGHT_BLUE_CANDLE,
-					Blocks.YELLOW_CANDLE,
-					Blocks.LIME_CANDLE,
-					Blocks.PINK_CANDLE,
-					Blocks.GRAY_CANDLE,
-					Blocks.LIGHT_GRAY_CANDLE,
-					Blocks.CYAN_CANDLE,
-					Blocks.PURPLE_CANDLE,
-					Blocks.BLUE_CANDLE,
-					Blocks.BROWN_CANDLE,
-					Blocks.GREEN_CANDLE,
-					Blocks.RED_CANDLE,
-					Blocks.BLACK_CANDLE
-			};
-
-			for (Block candleBlock: candleBlocks) {
-				EnchantmentPowerRegistry.register(
-						candleBlock
-								.getDefaultState()
-								.with(CandleBlock.CANDLES, 4)
-								.with(CandleBlock.LIT, true),
-						CONFIG.powerPerCandle() * 4
-				);
-				EnchantmentPowerRegistry.register(
-						candleBlock
-								.getDefaultState()
-								.with(CandleBlock.CANDLES, 3)
-								.with(CandleBlock.LIT, true),
-						CONFIG.powerPerCandle() * 3
-				);
-				EnchantmentPowerRegistry.register(
-						candleBlock
-								.getDefaultState()
-								.with(CandleBlock.CANDLES, 2)
-								.with(CandleBlock.LIT, true),
-						CONFIG.powerPerCandle() * 2
-				);
-				EnchantmentPowerRegistry.register(
-						candleBlock
-								.getDefaultState()
-								.with(CandleBlock.CANDLES, 1)
-								.with(CandleBlock.LIT, true),
-						CONFIG.powerPerCandle() * 1
-				);
-			}
-		}
 	}
 
 	public static class Tags {
