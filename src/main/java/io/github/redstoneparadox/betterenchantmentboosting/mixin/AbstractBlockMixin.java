de065bb93ebd3c7423cbc24123d4569067095660
RedstoneParadox <spinjitzu00@gmail.com>
2022-06-17 14:58:31 -0700

    Fixed AbstractBlock mixin targeting itself

--- src/main/java/io/github/redstoneparadox/betterenchantmentboosting/mixin/AbstractBlockMixin.java
+++ src/main/java/io/github/redstoneparadox/betterenchantmentboosting/mixin/AbstractBlockMixin.java
@@ -2,12 +2,13 @@ package io.github.redstoneparadox.betterenchantmentboosting.mixin;
 
 import io.github.redstoneparadox.betterenchantmentboosting.util.EnchantmentTableBooster;
 import io.github.redstoneparadox.betterenchantmentboosting.util.SearchUtil;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.tag.BlockTags;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 
-@Mixin(AbstractBlockMixin.class)
+@Mixin(AbstractBlock.class)
 public abstract class AbstractBlockMixin implements EnchantmentTableBooster {
 	@Override
 	public double getPower(World world, BlockState state) {
