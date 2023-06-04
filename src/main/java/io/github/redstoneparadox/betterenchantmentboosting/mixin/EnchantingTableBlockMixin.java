794508b188a5a52fd9e3c8942125922b8696f8a2
RedstoneParadox <spinjitzu00@gmail.com>
2023-06-04 13:44:22 -0700

    Enchantment particles now respect power
    Previously, unlit candles would still give off particles despite not providing power

--- src/main/java/io/github/redstoneparadox/betterenchantmentboosting/mixin/EnchantingTableBlockMixin.java
+++ src/main/java/io/github/redstoneparadox/betterenchantmentboosting/mixin/EnchantingTableBlockMixin.java
@@ -7,31 +7,39 @@ import net.minecraft.particle.ParticleTypes;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.random.RandomGenerator;
 import net.minecraft.world.World;
+import org.quiltmc.qsl.block.content.registry.api.BlockContentRegistries;
+import org.quiltmc.qsl.block.content.registry.api.enchanting.EnchantingBooster;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
+import java.util.Optional;
 
 @Mixin(EnchantingTableBlock.class)
 public abstract class EnchantingTableBlockMixin {
 	@Inject(method = "randomDisplayTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockWithEntity;randomDisplayTick(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/random/RandomGenerator;)V"), cancellable = true)
 	private void randomDisplayTick(BlockState state, World world, BlockPos pos, RandomGenerator random, CallbackInfo ci) {
-		List<BlockPos> bookshelfPositions = EnchantingUtil.search(world, pos);
-		double power = EnchantingUtil.getPower(world, bookshelfPositions);
+		List<BlockPos> boosterPositions = EnchantingUtil.search(world, pos);
+		double power = EnchantingUtil.getPower(world, boosterPositions);
 
 		if (power < 1.0) {
 			ci.cancel();
 			return;
 		}
 
-		for (BlockPos bookshelfPos: bookshelfPositions) {
+		for (BlockPos boosterPosition: boosterPositions) {
+			BlockState boosterState = world.getBlockState(boosterPosition);
+			Optional<EnchantingBooster> booster = BlockContentRegistries.ENCHANTING_BOOSTERS.get(state.getBlock());
+
+			if (booster.isEmpty()) continue;
+			if (booster.get().getEnchantingBoost(world, boosterState, pos) == 0) continue;
 			if (random.nextInt(16) != 0) continue;
 
-			int i = bookshelfPos.getX() - pos.getX();
-			int k = bookshelfPos.getY() - pos.getY();
-			int j = bookshelfPos.getZ() - pos.getZ();
+			int i = boosterPosition.getX() - pos.getX();
+			int k = boosterPosition.getY() - pos.getY();
+			int j = boosterPosition.getZ() - pos.getZ();
 
 			world.addParticle(
 					ParticleTypes.ENCHANT,
@@ -40,7 +48,8 @@ public abstract class EnchantingTableBlockMixin {
 					(double)pos.getZ() + 0.5,
 					(double)((float)i + random.nextFloat()) - 0.5,
 					(float)k - random.nextFloat() - 1.0f,
-					(double)((float)j + random.nextFloat()) - 0.5);
+					(double)((float)j + random.nextFloat()) - 0.5
+			);
 		}
 
 		ci.cancel();
