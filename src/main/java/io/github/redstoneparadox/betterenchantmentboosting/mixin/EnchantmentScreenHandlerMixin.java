01ae0db91e6d2c332a62dda3508f2d1288ac5eb5
RedstoneParadox <spinjitzu00@gmail.com>
2023-06-09 21:40:50 -0700

    Redirect for other call to gen enchantments
    Apparently mojang regenerates the enchantments when you press the enchantment button instead of caching anything.

--- src/main/java/io/github/redstoneparadox/betterenchantmentboosting/mixin/EnchantmentScreenHandlerMixin.java
+++ src/main/java/io/github/redstoneparadox/betterenchantmentboosting/mixin/EnchantmentScreenHandlerMixin.java
@@ -5,13 +5,15 @@ import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.EnchantmentLevelEntry;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.screen.EnchantmentScreenHandler;
 import net.minecraft.screen.Property;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.random.RandomGenerator;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -19,20 +21,27 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiConsumer;
 
 @Mixin(EnchantmentScreenHandler.class)
-public abstract class EnchantmentScreenHandlerMixin {
+public abstract class EnchantmentScreenHandlerMixin extends ScreenHandler {
 	@Unique ItemStack onContentChangedStack = ItemStack.EMPTY;
+	@Unique List<BlockPos> boosterPositions = new ArrayList<>();
 	@Shadow @Final private RandomGenerator random;
 	@Shadow @Final private Property seed;
+	@Shadow @Final public int[] enchantmentPower;
+	@Shadow @Final public int[] enchantmentId;
+	@Shadow @Final public int[] enchantmentLevel = new int[]{-1, -1, -1};
 
-	@Shadow
-	protected abstract List<EnchantmentLevelEntry> generateEnchantments(ItemStack stack, int slot, int level);
+	protected EnchantmentScreenHandlerMixin(@Nullable ScreenHandlerType<?> type, int syncId) {
+		super(type, syncId);
+	}
 
 	@Inject(method = "onContentChanged", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/inventory/Inventory;getStack(I)Lnet/minecraft/item/ItemStack;"))
 	private void onContentChanged_GetStack(Inventory inventory, CallbackInfo ci) {
@@ -44,47 +53,49 @@ public abstract class EnchantmentScreenHandlerMixin {
 		args.set(0, (BiConsumer<World, BlockPos>) this::bookshelfSearch);
 	}
 
+	@Redirect(method = "method_17410", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/screen/EnchantmentScreenHandler;generateEnchantments(Lnet/minecraft/item/ItemStack;II)Ljava/util/List;"))
+	private List<EnchantmentLevelEntry> redirectGenerateEnchantmentsCall(EnchantmentScreenHandler instance, ItemStack stack, int slot, int level) {
+		return EnchantingUtil.generateEnchantments(
+				stack,
+				slot,
+				enchantmentPower[slot],
+				random,
+				seed.get(),
+				world,
+				boosterPositions
+		);
+	}
+
 	private void bookshelfSearch(World world, BlockPos pos) {
-		EnchantmentScreenHandler self = (EnchantmentScreenHandler)(Object) this;
 		ItemStack stack = onContentChangedStack;
 		onContentChangedStack = ItemStack.EMPTY;
 
-		List<BlockPos> boosterPositions = EnchantingUtil.search(world, pos);
+		boosterPositions = EnchantingUtil.search(world, pos);
 		double power = EnchantingUtil.getPower(world, boosterPositions);
 
 		random.setSeed(seed.get());
 		for (int slot = 0; slot < 3; ++slot) {
-			self.enchantmentPower[slot] = EnchantmentHelper.calculateRequiredExperienceLevel(random, slot, (int) power, stack);
-			self.enchantmentId[slot] = -1;
-			self.enchantmentLevel[slot] = -1;
-			if (self.enchantmentPower[slot] >= slot + 1) continue;
-			self.enchantmentPower[slot] = 0;
+			enchantmentPower[slot] = EnchantmentHelper.calculateRequiredExperienceLevel(random, slot, (int) power, stack);
+			enchantmentId[slot] = -1;
+			enchantmentLevel[slot] = -1;
+			if (enchantmentPower[slot] >= slot + 1) continue;
+			enchantmentPower[slot] = 0;
 		}
 		for (int slot = 0; slot < 3; ++slot) {
 			List<EnchantmentLevelEntry> list = EnchantingUtil.generateEnchantments(
 					stack,
 					slot,
-					self.enchantmentPower[slot],
+					enchantmentPower[slot],
 					random,
 					seed.get(),
 					world,
 					boosterPositions
 			);
-			if (self.enchantmentPower[slot] <= 0 || list == null || list.isEmpty()) continue;
+			if (enchantmentPower[slot] <= 0 || list == null || list.isEmpty()) continue;
 			EnchantmentLevelEntry enchantmentLevelEntry = list.get(random.nextInt(list.size()));
-			self.enchantmentId[slot] = Registries.ENCHANTMENT.getRawId(enchantmentLevelEntry.enchantment);
-			self.enchantmentLevel[slot] = enchantmentLevelEntry.level;
+			enchantmentId[slot] = Registries.ENCHANTMENT.getRawId(enchantmentLevelEntry.enchantment);
+			enchantmentLevel[slot] = enchantmentLevelEntry.level;
 		}
-		self.sendContentUpdates();
-	}
-
-	private List<EnchantmentLevelEntry> generateEnchantments(ItemStack stack, int slot, int level, List<BlockPos> boosterPositions) {
-		random.setSeed(seed.get() + slot);
-		List<EnchantmentLevelEntry> list = EnchantmentHelper.generateEnchantments(this.random, stack, level, false);
-		if (stack.isOf(Items.BOOK) && list.size() > 1) {
-			list.remove(random.nextInt(list.size()));
-		}
-
-		return list;
+		sendContentUpdates();
 	}
 }
