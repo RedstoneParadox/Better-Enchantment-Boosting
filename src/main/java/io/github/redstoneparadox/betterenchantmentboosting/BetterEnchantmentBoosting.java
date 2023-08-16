c5fcb7cbce4fbb9676c4566f73b02db142bb0dad
RedstoneParadox <spinjitzu00@gmail.com>
2023-08-15 20:04:55 -0700

    Removed unused booster classes

--- src/main/java/io/github/redstoneparadox/betterenchantmentboosting/BetterEnchantmentBoosting.java
+++ src/main/java/io/github/redstoneparadox/betterenchantmentboosting/BetterEnchantmentBoosting.java
@@ -4,8 +4,6 @@ import io.github.redstoneparadox.betterenchantmentboosting.config.BetterEnchantm
 import io.github.redstoneparadox.betterenchantmentboosting.data.ActivationCountEnchantingBooster;
 import io.github.redstoneparadox.betterenchantmentboosting.data.ActivationEnchantingBooster;
 import io.github.redstoneparadox.betterenchantmentboosting.data.InventoryEnchantingBooster;
-import io.github.redstoneparadox.betterenchantmentboosting.util.CandleBooster;
-import io.github.redstoneparadox.betterenchantmentboosting.util.ChiseledBookshelfBooster;
 import net.minecraft.block.Blocks;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.tag.TagKey;
@@ -15,7 +13,6 @@ import org.apache.logging.log4j.Logger;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.quiltmc.qsl.block.content.registry.api.BlockContentRegistries;
-import org.quiltmc.qsl.block.content.registry.api.enchanting.EnchantingBoosterType;
 
 public final class BetterEnchantmentBoosting implements ModInitializer {
 	public static final String MODID = "betterenchantmentboosting";
